372078835d0cb03a8d37901df98a6953d07d0a26
umer2001 <umer2001.uf@gmail.com>
2024-06-22 06:18:17 +0000

    feat: soft-delete

---
+++ frappe/custom/doctype/soft_delete/soft_delete.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2024, Frappe Technologies and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class SoftDelete(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+		from webshop.webshop.doctype.soft_delete_list.soft_delete_list import SoftDeleteList
+
+		deleted_items: DF.Table[SoftDeleteList]
+		doc_type: DF.Link | None
+	# end: auto-generated types
+	pass
